61771eb3922209cd0644753306ad229f54be7afd
dewaleolaoye <adewaleolaoye21@gmail.com>
2020-02-11 16:04:34 +0100

    setup form

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Chat App`,
+    description: `Twilio Chat Api with React`,
+    author: `dewaleolaoye`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/components/join.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react"
+// import useTwilioVideo from "../hooks/use-twilio"
+// import { navigate } from "gatsby"
+
+const Join = ({ location }) => {
+  // const defaultRoom =
+  //   (location && location.state && location.state.roomName) || '';
+  // const defaultSession =
+  //   (location && location.state && location.state.sessionId) || null;
+  // const { state, getRoomToken } = useTwilioVideo();
+  // const [identity, setIdentity] = useState('');
+  // const [roomName, setRoomName] = useState(defaultRoom);
+
+  // useEffect(() => {
+  //   if (state.token && state.roomName) {
+  //     navigate(`/room/${state.roomName}/${state.sessionId}`);
+  //   }
+  // }, [state]);
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    // getRoomToken({ identity, roomName, sessionId: defaultSession });
+  }
+
+  return (
+    <>
+      <h1>Start or Join Chat</h1>
+
+      {/* <pre>{JSON.stringify(state, null, 2)}</pre> */}
+      <form className="start-form" onSubmit={handleSubmit}>
+        <label htmlFor="identity">
+          Display Name:
+          <input
+            type="text"
+            id="identity"
+            // value={identity}
+            // onChange={event => setIdentity(event.target.value)}
+          />
+        </label>
+        <label htmlFor="roomName">
+          Which room do you want to join?
+          <input
+            type="text"
+            id="roomName"
+            // value={roomName}
+            // onChange={event => setRoomName(event.target.value)}
+          />
+        </label>
+        <button type="submit">Join Video Chat</button>
+      </form>
+    </>
+  )
+}
+
+export default Join
